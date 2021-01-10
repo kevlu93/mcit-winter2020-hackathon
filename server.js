7a548c2acfaf337fcd5f16741ecb7bacb11c65dd
camf87 <clafer@seas.upenn.edu>
2021-01-10 19:49:20 +0100

    added memorystore require

--- server.js
+++ server.js
@@ -12,6 +12,9 @@ const morgan = require('morgan');
 const cookieParser = require('cookie-parser');
 const bodyParser = require('body-parser');
 const session = require('express-session');
+const session = require('express-session');
+const MemoryStore = require('memorystore')(session);
+
 
 // //const appDb = require('./config/database.js');
 // appDbUrl = "mongodb://localhost:27017/todolistDB"
@@ -21,7 +24,7 @@ const session = require('express-session');
 // Coment out when testing locally and replace with above
 const user = process.env.user;
 const password = process.env.password;
-appDbUrl = "mongodb+srv://" + user + ":" + password + "@clusterdefault.faspm.mongodb.net/tododb?retryWrites=true&w=majority"
+appDbUrl = "mongodb+srv://" + user + ":" + password + "@clusterdefault.faspm.mongodb.net/tododb?retryWrites=true&w=majority";
 
 // appDbUrl = "mongodb://"+user+":"+ password + "@clusterdefault-shard-00-00.faspm.mongodb.net:27017,clusterdefault-shard-00-01.faspm.mongodb.net:27017,clusterdefault-shard-00-02.faspm.mongodb.net:27017/tododb?ssl=true&replicaSet=atlas-p28bkx-shard-0&authSource=admin&retryWrites=true&w=majority"
 
